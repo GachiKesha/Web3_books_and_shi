bb3174f6a8aaf4b11e9060ab38b15e877ac9b67a
GachiKesha <stukalin.vlad@gmail.com>
2025-05-19 12:26:07 +0300

    fix: add methods to cors

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   // enable cors
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    methods: ['GET', 'POST'],
     credentials: true,
   });
 
